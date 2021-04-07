8e0d31d2ef9d3d454fc0d6eca602714c66185726
Taylor Keazirian <taylork26@gmail.com>
2021-04-06 19:57:45 -0500

    added fetch call instead of using context

--- src/Goal/Goal.js
+++ src/Goal/Goal.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import ApiContext from '../Context/ApiContext';
 import config from '../config';
 import Moment from 'react-moment';
 import { categories } from '../constants/categories';
 
 class Goal extends React.Component {
-	static contextType = ApiContext;
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: '',
+			goal_name: '',
+			category: '',
+			notes: '',
+			post_date: '',
+		};
+	}
+
+	componentDidMount() {
+		const goalId = this.props.match.params.id;
+		fetch(`${config.API_BASE_URL}/goals/${goalId}`, {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json',
+			},
+		})
+			.then(res => {
+				if (!res.ok) {
+					return res.json().then(error => Promise.reject(error));
+				}
+				return res.json();
+			})
+			.then(goal => {
+				this.setState({
+					id: goal.id,
+					goal_name: goal.goal_name,
+					category: goal.category,
+					notes: goal.notes,
+					post_date: goal.post_date,
+				});
+			});
+	}
 
 	handleClickGoBack = () => {
 		this.props.history.push('/dashboard');
@@ -27,30 +60,29 @@ class Goal extends React.Component {
 	};
 
 	render() {
-		const oneGoal = this.context.goals.find(
-			goal => goal.id === parseInt(this.props.match.params.id)
-		);
+
+		const { id, goal_name, category, notes, post_date } = this.state;
 
 		return (
 			<div className='goals-container'>
 				<div className='goals-list'>
 					<div className='one-goal-item'>
-						<div key={oneGoal.id} className='goal-item-info'>
-							<h2 className='goals-header'>{oneGoal.goal_name}</h2>
+						<div key={id} className='goal-item-info'>
+							<h2 className='goals-header'>{goal_name}</h2>
 							<div className='category-div'>
 								<h4>Category:</h4>
-								<span className='category'>{categories[oneGoal.category]}</span>
+								<span className='category'>{categories[category]}</span>
 							</div>
 							<div className='note-div'>
 								<h4>Notes:</h4>
-								<span className='goal-note'>{oneGoal.notes}</span>
+								<span className='goal-note'>{notes}</span>
 							</div>
 							<div className='date-div'>
 								<h4>Date Added:</h4>
-								<Moment format='dddd MMM D YYYY'>{oneGoal.post_date}</Moment>
+								<Moment format='dddd MMM D YYYY'>{post_date}</Moment>
 							</div>
 							<div className='button-container'>
-								<Link to={`/edit-goal/${oneGoal.id}`}>
+								<Link to={`/edit-goal/${id}`}>
 									<button type='submit' className='edit-goal-btn'>
 										Edit
 									</button>
